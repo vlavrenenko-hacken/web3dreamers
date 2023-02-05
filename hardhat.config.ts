5b32b0e4a10fca1f9babec5507871b1ad212fda3
Viktor Lavrenenko <lavrenenkoviktoren@gmail.com>
2023-02-05 14:23:58 +0200

    Add hardhat config

---
+++ hardhat.config.ts
@@ -0,0 +1,22 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import "hardhat-gas-reporter";
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: "0.8.13",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  gasReporter: {
+    enabled: true
+  }
+
+};
+
+
+export default config;
